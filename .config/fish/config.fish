010d1e9cf6f5e9046525caae7d915ceee15947d5
hvmegy <hamegy08lleo@gmail.com>
2024-07-09 12:04:35 +0700

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,27 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    theme_gruvbox dark medium
+end
+
+set fish_greeting ""
+
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+if [ (tty) = "/dev/tty1" ]
+    exec Hyprland
+end
+
+if test (tty) = "/dev/tty1"
+    exec Hyprland
+end
+
+set -gx EDITOR nvim
+
+starship init fish | source
