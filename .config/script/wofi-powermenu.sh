e19db897f6f34dc2dc039c54f22311e70b79e2a9
hvmegy <hamegy08lleo@gmail.com>
2024-08-31 19:30:02 +0700

    powermenu sh

---
+++ .config/script/wofi-powermenu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+op=$( echo -e " Poweroff\n Reboot\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+
+case $op in 
+        poweroff)
+                ;&
+        reboot)
+		;&
+        lock)
+		hyprlock 
+                ;;
+        logout)
+		hyprctl dispatcher exit
+                ;;
+esac
